7b7a67086b59bfabb42e498bbb8465eedce4262d
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-01 14:35:02 +0100

    Make the hero section responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 export default function Hero() {
 	return (
-		<section className="w-full h-screen bg-slate-100 p-8 pt-24 grid grid-cols-3 grid-rows-6 font-light rounded-2xl text-slate-700">
+		<section className="w-full h-screen bg-slate-100 p-8 px-8 lg:px-32  pt-24 grid grid-cols-3 grid-rows-6 font-light rounded-b-2xl text-slate-700">
 			<p className="col-start-1 col-end-4 text-5xl lg:text-6xl font-semibold max-w-2xl text-slate-700 w-full h-fit">
 				Full stack Software Engineer on a mission to build simple solutions to
 				complex real-world problems.
@@ -16,20 +17,20 @@ export default function Hero() {
 				Based in <br /> Lagos, Nigeria.
 			</div>
 			<div className="flex gap-2 justify-center row-start-6">
-				<a
+				<Link
 					href="https://www.linkedin.com/in/somtochukwu-francis-b8a236239"
 					target="_blank"
-					className="hover:underline"
+					className="h-fit"
 				>
 					LinkedIn
-				</a>
-				<a
+				</Link>
+				<Link
 					href="https://twitter.com/somtofrancis3"
 					target="_blank"
-					className="hover:underline"
+					className="h-fit"
 				>
 					Twitter
-				</a>
+				</Link>
 			</div>
 		</section>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,22 @@
+import Link from "next/link";
+
 export default function Navbar() {
 	return (
-		<nav className="flex justify-between items-center text-white mix-blend-difference font-light z-10 h-[10vh] box-border px-8 fixed w-full max-w-screen-2xl bg-transparent">
+		<nav className="flex justify-between items-center text-white mix-blend-difference font-light z-10 h-[10vh] box-border px-8 lg:px-32 fixed w-full max-w-screen-2xl bg-transparent">
 			<div>
-				Francis Somtochukwu <span className="hidden sm:inline">Jeremy</span>
+				<Link href={"/"}>&copy; somtochukwu</Link>
 			</div>
 			<ul className="flex justify-between list-none gap-5">
 				<li>
-					<a href="#projects" className="hover:underline">
-						Projects
-					</a>
+					<Link href="#projects">Projects</Link>
 				</li>
 				<li>
-					<a href="#info" className="hover:underline">
-						Info
-					</a>
+					<Link href="#info">Info</Link>
 				</li>
 				<li>
-					<a
-						href="https://rewrite-blog.vercel.app"
-						target="_blank"
-						className="hover:underline"
-					>
+					<Link href="https://rewrite-blog.vercel.app" target="_blank">
 						Blog
-					</a>
+					</Link>
 				</li>
 			</ul>
 		</nav>
