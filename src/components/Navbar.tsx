28e76202d9cec4dcda17783b9ad6f93087482bbe
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-18 17:06:10 +0100

    make brutalist

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import Burger from "./Burger";
+import ThemeSwitcher from "./ThemeSwitcher";
+
+const links = [
+	{ name: "Projects", href: "#projects" },
+	{ name: "Info", href: "#info" },
+	{ name: "Blog", href: "https://rewrite-blog.vercel.app", target: "_blank" },
+];
 
 export default function Navbar() {
 	return (
 		<>
-			<nav className="flex justify-between items-center text-white mix-blend-difference font-light z-10 h-[10vh] box-border px-8 lg:px-32 fixed w-full max-w-screen-2xl bg-transparent">
+			<nav className="flex justify-between items-center text-slate-700  font-light z-10 h-[10vh] box-border px-8 lg:px-32 w-full max-w-screen-2xl bg-transparent border-b-2 border-slate-700">
 				<div>
 					<Link href={"/"}>&copy; somtochukwu</Link>
 				</div>
-				<ul className="flex justify-between list-none gap-5">
-					<li>
-						<Link href="#projects" className="after:bg-white">
-							Projects
-						</Link>
-					</li>
-					<li>
-						<Link href="#info" className="after:bg-white">
-							Info
-						</Link>
-					</li>
-					<li>
-						<Link
-							href="https://rewrite-blog.vercel.app"
-							target="_blank"
-							className="after:bg-white"
-						>
-							Blog
-						</Link>
-					</li>
+				<ul className="flex items-center list-none h-full font-bold text-xs gap-5">
+					{links.map((link) => {
+						return (
+							<li className="after:bg-white h-full flex justify-center items-center last-of-type:border-none capitalize">
+								<Link href={link.href} target={link.target}>
+									{link.name.toUpperCase()}
+								</Link>
+							</li>
+						);
+					})}
+					<ThemeSwitcher />
 				</ul>
 			</nav>
 			<Burger />
