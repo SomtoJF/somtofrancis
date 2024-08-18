35b883e90773fc399fd9fcf78e785690e4ce0491
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-18 19:30:22 +0100

    Add theme provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { ReactLenis } from "lenis/react";
+import { ThemeProvider } from "next-themes";
 import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return <ReactLenis root>{children}</ReactLenis>;
+	return (
+		<ReactLenis root>
+			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+				{children}
+			</ThemeProvider>
+		</ReactLenis>
+	);
 }
