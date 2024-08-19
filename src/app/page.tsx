3610c4f9bbb6445e3dd4b56b0246d189a2900df9
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-19 01:12:40 +0100

    stylefix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@
 
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import { useEffect, useState } from "react";
 // import Experience from "@/components/Experience";
 // import Projects from "@/components/Projects";
 // import ProjectsV2 from "@/components/ProjectsV2";
 import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
+	const [windowWidth, setWindowWidth] = useState<number>(0);
+	useEffect(() => {
+		setWindowWidth(window.innerWidth);
+	}, []);
 	return (
 		<main className="w-full max-w-screen-2xl">
-			{window.innerWidth > 650 && (
+			{windowWidth > 650 && (
 				<AnimatedCursor
 					color="0,0,0,0"
 					innerSize={5}
