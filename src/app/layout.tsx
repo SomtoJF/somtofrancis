88adbdab143d7631f32aa81d1e6d0b7d0b3b9656
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-20 22:45:25 +0100

    Bugfix: Website doesnt scroll on mobike

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} w-screen overflow-hidden`}>
+			<body className={`${inter.className} w-screen`}>
 				<Providers>
 					<div className="w-screen min-h-screen bg-slate-100 text-slate-700 2xl:bg-slate-100 flex justify-center">
 						{children}
