4ae4e8295333a44d2c58e323afb496031f4722b4
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-08-20 22:22:18 +0100

    Build: uninstall lenis

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { ReactLenis } from "lenis/react";
 import { ThemeProvider } from "next-themes";
 import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<ReactLenis root>
-			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-				{children}
-			</ThemeProvider>
-		</ReactLenis>
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+			{children}
+		</ThemeProvider>
 	);
 }
